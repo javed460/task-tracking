eb56beff8ce7da1286f761652ea9f6b45a8d8e2b
Javed Bagwan <javed460@gmail.com>
2023-08-13 09:43:02 +0530

    Initial Project Structure

---
+++ src/main/java/com/example/tasktraking/TaskTrakingApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasktraking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrakingApplication.class, args);
+	}
+
+}
