14f69a8878b19dd154b5839a180bc6af7440a4ad
Javed Bagwan <javed460@gmail.com>
2023-08-16 08:08:00 +0530

    Added Transformer

---
+++ src/main/java/com/example/tasktraking/transformer/TaskTransformer.java
@@ -0,0 +1,32 @@
+package com.example.tasktraking.transformer;
+
+import com.example.tasktraking.dto.TaskDTO;
+import com.example.tasktraking.entity.Task;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class TaskTransformer {
+
+    public static List<TaskDTO> transformToTaskList(List<Task> taskList) {
+        return taskList.stream()
+                .map(TaskTransformer::transformToTaskDTO)
+                .collect(Collectors.toList());
+    }
+
+    private static TaskDTO transformToTaskDTO(Task task) {
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setTitle(task.getTitle());
+        taskDTO.setDescription(task.getDescription());
+        taskDTO.setDueDate(task.getDueDate());
+        return taskDTO;
+    }
+
+    public static Task transformToTask(TaskDTO taskDTO) {
+        Task task = new Task();
+        task.setTitle(taskDTO.getTitle());
+        task.setDescription(taskDTO.getDescription());
+        task.setCompleted(taskDTO.isCompleted());
+        return task;
+    }
+}
