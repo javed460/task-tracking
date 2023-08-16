cdcf81f6fb1108467d982028682a59c8767864df
Javed Bagwan <javed460@gmail.com>
2023-08-16 08:08:51 +0530

    Controller and service

--- src/main/java/com/example/tasktraking/controller/TaskController.java
+++ src/main/java/com/example/tasktraking/controller/TaskController.java
@@ -1,10 +1,16 @@
 package com.example.tasktraking.controller;
 
+import com.example.tasktraking.dto.TaskDTO;
+import com.example.tasktraking.entity.Task;
 import com.example.tasktraking.service.TaskService;
+import com.example.tasktraking.transformer.TaskTransformer;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.example.tasktraking.transformer.TaskTransformer.transformToTask;
+import static com.example.tasktraking.transformer.TaskTransformer.transformToTaskList;
 
 @RestController
 @RequestMapping("/tasks")
@@ -12,13 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController {
     private final TaskService taskService;
 
-    /*@GetMapping
-    public List<Task> getAllTasks() {
-        return taskService.findAll();
+    @GetMapping
+    public List<TaskDTO> getAllTasks() {
+        List<Task> taskList = taskService.findAll();
+        return transformToTaskList(taskList);
     }
 
     @PostMapping
-    public Task createTask(@RequestBody Task task) {
-        return taskService.save(task);
-    }*/
+    public Integer createTask(@RequestBody TaskDTO taskDTO) {
+        Task task = transformToTask(taskDTO);
+        return taskService.create(task);
+    }
 }
--- src/main/java/com/example/tasktraking/service/TaskService.java
+++ src/main/java/com/example/tasktraking/service/TaskService.java
@@ -1,11 +1,23 @@
 package com.example.tasktraking.service;
 
+import com.example.tasktraking.entity.Task;
 import com.example.tasktraking.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
     private final TaskRepository taskRepository;
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public Integer create(Task task) {
+        Task savedTask = taskRepository.save(task);
+        return savedTask.getId().intValue();
+    }
 }
